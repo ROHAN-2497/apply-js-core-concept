e2f3e51d7b7fc7dcdfcfbb7307de0b5949bbc117
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-11 00:33:45 +0600

    inche to feet files

---
+++ inchiToFeet.js
@@ -0,0 +1,13 @@
+const myInchi = 12;
+const feet = myInchi / 12;
+console.log(feet)
+
+
+const dadaInche = 144;
+const dadaFeet = dadaInche / 12;
+console.log("dada height", dadaFeet);
+
+const dadiInche = 60;
+const dadiFeet  = dadiInche / 12;
+console.log('dadir height', dadiFeet);
+
